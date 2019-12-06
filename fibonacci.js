eccf739b971458133957326437d4843aec330741
Raghav Goyal <rgq5943@lainpuma507.springernature.com>
2019-12-06 16:51:08 +0530

    req. 2 & 3 completed

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,7 @@
 function fibonacci(no_of_elements)
 {
 	
-	var array_ret= new Array(no_of_elements);
+	var array_ret= new Array();
 	array_ret[0]=1;
 	array_ret[1]=1;
 	for(var i=2; i<no_of_elements; i++)
